43a57efafe80fd56d3cc8565dbae663ff386ef9c
FatalMistake-hub <79819674+FatalMistake-hub@users.noreply.github.com>
2022-10-01 22:58:52 +0700

    A

--- src/App.js
+++ src/App.js
@@ -5,35 +5,34 @@ import DefaultLayout from '~/layouts';
 
 function App() {
     return (
-        // <Router>
-        //     <div className="App">
-        //         <Routes>
-        //             {publicRoutes.map((route, index) => {
-        //                 const Page = route.component;
-        //                 let Layout = DefaultLayout;
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
 
-        //                 if (route.layout) {
-        //                     Layout = route.layout;
-        //                 } else if (route.layout === null) {
-        //                     Layout = Fragment;
-        //                 }
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
 
-        //                 return (
-        //                     <Route
-        //                         key={index}
-        //                         path={route.path}
-        //                         element={
-        //                             <Layout>
-        //                                 <Page />
-        //                             </Layout>
-        //                         }
-        //                     />
-        //                 );
-        //             })}
-        //         </Routes>
-        //     </div>
-        // </Router>
-        <h2 className="">hello</h2>
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
